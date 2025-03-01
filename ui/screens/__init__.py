15b5d09d5e9d332deb080b29f8ad8062bb227aae
mxuAWD23VuPTDM2 <kagirimoffat@gmail.com>
2025-03-01 14:27:17 +0300

    Cleaned branch to keep only Android-related files

--- ui/screens/__init__.py
+++
@@ -1,7 +0,0 @@
-# ui/screens/__init__.py
-from .home import HomeScreen
-from .loans import LoansScreen
-from .inputs import InputScreen
-from .summaries import SummariesScreen
-
-__all__ = ['HomeScreen', 'LoansScreen', 'InputScreen', 'SummariesScreen']
